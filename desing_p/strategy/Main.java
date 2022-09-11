dc8ec4a84c26b4009d05c0f68b2267c46677732c
Deepak <blrhyd0@gmail.com>
2022-09-11 15:01:24 +0530

    Delete desing_p/strategy directory

--- desing_p/strategy/Main.java
+++
@@ -1,9 +0,0 @@
-package com.practice.desing_p.strategy;
-
-public class Main {
-	public static void main(String[] args) {
-		Context context = new Context(new CalculateSub());
-		//Context context = new Context(new CalculateSum());
-		context.executeStrategy();
-	}
-}
--- desing_p/strategy/Strategy.java
+++
@@ -1,6 +0,0 @@
-package com.practice.desing_p.strategy;
-
-public interface Strategy {
-	
-	public void doOperation();
-}
